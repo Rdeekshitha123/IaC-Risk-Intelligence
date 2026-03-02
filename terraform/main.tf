862acb00751948d5dddda27a348367fbc69860d5
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-03-02 14:31:36 +0530

    Clean terraform - no violations

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Intentional violation: SSH open to world
-resource "aws_security_group" "test_sg" {
-  name = "test-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "test_instance" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
@@ -24,5 +12,3 @@ resource "aws_instance" "test_instance" {
     CostCenter  = "eng"
   }
 }
-
-# Intentional violation: Unencrypted S3 bucket
