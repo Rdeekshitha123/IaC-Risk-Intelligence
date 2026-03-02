662fe1e0cfc81f69c1062a7ee7a8277f22c1067b
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-03-02 14:50:17 +0530

    Clean terraform - no violations044

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Intentional violation - SSH open to world
+resource "aws_security_group" "test_sg" {
+  name = "test-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "test_instance" {
-  ami               = "ami-0c55b159cbfafe1f0"
-  instance_type     = "t3.micro"
-  ebs_optimized     = true
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t3.micro"
+  ebs_optimized = true
 
   metadata_options {
-    http_tokens               = "required"
-    http_endpoint             = "enabled"
+    http_tokens                 = "required"
+    http_endpoint               = "enabled"
     http_put_response_hop_limit = 1
   }
 
