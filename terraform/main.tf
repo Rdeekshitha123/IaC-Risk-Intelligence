9b2cfcd8f12511a2c980cd7247bc7704f277b390
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-03-02 16:00:40 +0530

    Clean terraform - no violations02

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "test_sg" {
+  name = "test-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "test_instance" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
   ebs_optimized = true
-
   metadata_options {
     http_tokens                 = "required"
     http_endpoint               = "enabled"
     http_put_response_hop_limit = 1
   }
-
   root_block_device {
     encrypted = true
   }
-
   tags = {
     Environment = "dev"
     Owner       = "team"
