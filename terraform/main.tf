ea1fa295e68e85982e8eb855a31d4bb7558c41a8
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-03-02 14:36:21 +0530

    Clean terraform - no violations000

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,11 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
 
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
+
   tags = {
     Environment = "dev"
     Owner       = "team"
